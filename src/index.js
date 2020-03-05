d5e896bb418a61a44c7737e035c7f19bb41d4a47
Wilson <wilson.neto@nerdweb.com.br>
2020-03-05 10:37:45 -0300

    Persisting state

--- src/index.js
+++ src/index.js
@@ -9,11 +9,43 @@ const App = () => {
   const [state, setState] = useState(initialData);
   const onDragEnd = result => {
     // TODO: reorder our column
+    const { destination, source, draggableId } = result;
+
+    if (!destination) {
+      return;
+    }
+
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+
+    const column = state.columns[source.droppableId];
+    const newTaskIds = Array.from(column.taskIds);
+    newTaskIds.splice(source.index, 1);
+    newTaskIds.splice(destination.index, 0, draggableId);
+
+    const newColumn = {
+      ...column,
+      taskIds: newTaskIds
+    };
+
+    const newState = {
+      ...state,
+      columns: {
+        ...state.columns,
+        [newColumn.id]: newColumn
+      }
+    };
+
+    setState(newState);
   };
   return (
     <DragDropContext
       // Only required attribute, the responsability is to update the state
-      onDragEnd
+      onDragEnd={onDragEnd}
     >
       {state.columnOrder.map(columnId => {
         const column = state.columns[columnId];
